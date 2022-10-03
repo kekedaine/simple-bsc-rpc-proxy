f463ff7c1834d5ab21e4c3ff46a9686b7530978f
Dai Nguyen <daidnv@gmail.com>
2022-10-04 00:49:40 +0700

    minor update

--- src/utils/configs.js
+++ src/utils/configs.js
@@ -6,4 +6,5 @@ module.exports = {
     SERVICE_NAME: process.env.SERVICE_NAME,
     NODE_ENV: process.env.NODE_ENV,
     RPC_ORIGIN: process.env.RPC_ORIGIN,
+    IS_ENABLE_ETHERJS: process.env.IS_ENABLE_ETHERJS === 'true',
 }
