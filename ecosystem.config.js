2df06b5a7f1ab7e4ec6b6bdc12908defae8c187e
Dai Nguyen <daidnv@gmail.com>
2022-10-04 01:10:11 +0700

    minor update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,14 +3,29 @@ module.exports = {
         {
             name: 'rpc_proxy_service',
             max_memory_restart: '900M',
-            port: 3000,
             autorestart: true,
             script: './src/index.js',
             out_file: '/dev/null',
             error_file: '/dev/null',
+            env_bsc: {
+                PORT: 3000,
+                NODE_ENV: 'production',
+                SERVICE_NAME: 'bsc-rpc',
+                RPC_ORIGIN: 'https://bsc-dataseed.binance.org',
+                IS_ENABLE_ETHERJS: 'true',
+            },
+            env_arb: {
+                PORT: 3001,
+                NODE_ENV: 'production',
+                SERVICE_NAME: 'arb-rpc',
+                RPC_ORIGIN: 'https://bsc-dataseed.binance.org',
+                IS_ENABLE_ETHERJS: 'true',
+            },
         },
     ],
 }
+// pm2 start ecosystem.config.js --env bsc
+// pm2 start ecosystem.config.js --env arb
 // pm2 start ecosystem.config.js
 // pm2 restart
 // pm2 stop all
