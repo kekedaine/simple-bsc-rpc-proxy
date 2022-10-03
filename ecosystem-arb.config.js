03eba26bd10af233edc1a4e1163d234fd862c71f
Dai Nguyen <daidnv@gmail.com>
2022-10-04 01:15:01 +0700

    minor update

--- ecosystem-arb.config.js
+++ ecosystem-arb.config.js
@@ -1,20 +1,13 @@
 module.exports = {
     apps: [
         {
-            name: 'rpc_proxy_service',
+            name: 'rpc_proxy_service_arb',
             max_memory_restart: '900M',
             autorestart: true,
             script: './src/index.js',
             out_file: '/dev/null',
             error_file: '/dev/null',
-            env_bsc: {
-                PORT: 3000,
-                NODE_ENV: 'production',
-                SERVICE_NAME: 'bsc-rpc',
-                RPC_ORIGIN: 'https://bsc-dataseed.binance.org',
-                IS_ENABLE_ETHERJS: 'true',
-            },
-            env_arb: {
+            env: {
                 PORT: 3001,
                 NODE_ENV: 'production',
                 SERVICE_NAME: 'arb-rpc',
@@ -24,8 +17,8 @@ module.exports = {
         },
     ],
 }
-// pm2 start ecosystem.config.js --env bsc
-// pm2 start ecosystem.config.js --env arb
+// pm2 start ecosystem-bsc.config.js
+// pm2 start ecosystem-arb.config.js
 // pm2 start ecosystem.config.js
 // pm2 restart
 // pm2 stop all
---
+++ ecosystem-bsc.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    apps: [
+        {
+            name: 'rpc_proxy_service_bsc',
+            max_memory_restart: '900M',
+            autorestart: true,
+            script: './src/index.js',
+            out_file: '/dev/null',
+            error_file: '/dev/null',
+            env: {
+                PORT: 3000,
+                NODE_ENV: 'production',
+                SERVICE_NAME: 'bsc-rpc',
+                RPC_ORIGIN: 'https://bsc-dataseed.binance.org',
+                IS_ENABLE_ETHERJS: 'true',
+            },
+        },
+    ],
+}
+// pm2 start ecosystem.config.js --env bsc
+// pm2 start ecosystem.config.js --env arb
+// pm2 start ecosystem.config.js
+// pm2 restart
+// pm2 stop all
+// pm2 delete all
